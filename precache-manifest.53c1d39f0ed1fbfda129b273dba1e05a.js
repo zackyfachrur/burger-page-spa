c1f072c9de98c97b7489cbe5622c40ce567a0880
zackyfachrur <mochzackyaja@gmail.com>
2024-05-07 01:35:09 +0700

    Updates

---
+++ precache-manifest.53c1d39f0ed1fbfda129b273dba1e05a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ac22948a6163ad86b6d76680619199",
+    "url": "/burger-page/index.html"
+  },
+  {
+    "revision": "a9df12aed2300e06bb35",
+    "url": "/burger-page/static/css/2.9c1d453f.chunk.css"
+  },
+  {
+    "revision": "be2f5748db13bd9e4020",
+    "url": "/burger-page/static/css/main.af625303.chunk.css"
+  },
+  {
+    "revision": "a9df12aed2300e06bb35",
+    "url": "/burger-page/static/js/2.59b92499.chunk.js"
+  },
+  {
+    "revision": "21d8eb0bed2f26e73cb4",
+    "url": "/burger-page/static/js/3.b1b92a21.chunk.js"
+  },
+  {
+    "revision": "be2f5748db13bd9e4020",
+    "url": "/burger-page/static/js/main.8ed778ee.chunk.js"
+  },
+  {
+    "revision": "afd26f2d54e424397896",
+    "url": "/burger-page/static/js/runtime~main.5727abf5.js"
+  },
+  {
+    "revision": "09b5a4eec65c7548c90725dda8232292",
+    "url": "/burger-page/static/media/burger-about-2.09b5a4ee.png"
+  },
+  {
+    "revision": "54c2c9209959563698937f31702451e5",
+    "url": "/burger-page/static/media/burger-about-3.54c2c920.png"
+  },
+  {
+    "revision": "95ad0f3b0196a89c90d842df7bf54592",
+    "url": "/burger-page/static/media/burger-about.95ad0f3b.jpg"
+  },
+  {
+    "revision": "dd388e037cb0a107cb2a35f850ca0219",
+    "url": "/burger-page/static/media/burger-top.dd388e03.png"
+  },
+  {
+    "revision": "c64549f727cda49838a61ec4a58e34aa",
+    "url": "/burger-page/static/media/chef-character.c64549f7.jpg"
+  },
+  {
+    "revision": "9611107eb0b7daa197b5a983a7cf8374",
+    "url": "/burger-page/static/media/chef-contact.9611107e.jpg"
+  },
+  {
+    "revision": "ad6aef01d2761a474ac12a84cbdff9d6",
+    "url": "/burger-page/static/media/chef-home.ad6aef01.jpg"
+  },
+  {
+    "revision": "e442fc63bc04280de690b8e6bf929f10",
+    "url": "/burger-page/static/media/orange.e442fc63.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/burger-page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/burger-page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/burger-page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/burger-page/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "088b453f9b8f855f745d9d099edce8f2",
+    "url": "/burger-page/static/media/strawberry.088b453f.png"
+  },
+  {
+    "revision": "7a3a593b5308dc039bf0db4a904004eb",
+    "url": "/burger-page/static/media/tea.7a3a593b.png"
+  },
+  {
+    "revision": "2b1e46b637de7f4e8892ec8764313b53",
+    "url": "/burger-page/static/media/water.2b1e46b6.png"
+  }
+]);
